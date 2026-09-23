a8532af70cd41d6cf38ce122dfa0cc65bb07b7f6
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-09-24 02:34:15 +0530

    Add First Non Repeating Character solution

---
+++ src/main/java/string/class_problems/FirstNonRepeatingCharacter.java
@@ -0,0 +1,38 @@
+package string.class_problems;
+
+import java.util.Scanner;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        char result = '\0';
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                result = ch;
+                break;
+            }
+        }
+
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found");
+        }
+
+        sc.close();
+    }
+}
