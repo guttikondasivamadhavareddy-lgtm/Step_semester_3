556626a657d99b315d11e2c11c428b0994cebdf5
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-09-25 00:41:09 +0530

    Fix word reversal encoder

--- src/main/java/string/assigment_problems/WordReversalEncoder.java
+++ src/main/java/string/assigment_problems/WordReversalEncoder.java
@@ -3,32 +3,30 @@ package string.assigment_problems;
 import java.util.Scanner;
 
 public class WordReversalEncoder {
+
+    public static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversedWord = new StringBuilder(words[i]);
+            result.append(reversedWord.reverse());
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a sentence: ");
         String sentence = sc.nextLine();
 
-        String[] words = sentence.split("\\s+");
-
-        System.out.print("Encoded sentence: ");
-
-        for (int i = words.length - 1; i >= 0; i--) {
-            String word = words[i];
-            String reverse = "";
-
-            for (int j = word.length() - 1; j >= 0; j--) {
-                reverse += word.charAt(j);
-            }
-
-            System.out.print(reverse);
-
-            if (i > 0) {
-                System.out.print(" ");
-            }
-        }
-
-        System.out.println();
+        System.out.println("Encoded sentence: " + reverseEachWord(sentence));
 
         sc.close();
     }
