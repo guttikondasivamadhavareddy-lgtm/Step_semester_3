ddef329c5e2190f5e25d44f661464bcb97765108
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-09-24 15:54:52 +0530

    Add Warehouse Inventory Balancer solution

---
+++ src/main/java/string/assigment_problems/WarehouseInventoryBalancer.java
@@ -0,0 +1,44 @@
+package string.assigment_problems;
+
+import java.util.Scanner;
+
+public class WarehouseInventoryBalancer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of warehouses: ");
+        int n = sc.nextInt();
+
+        int[] inventory = new int[n];
+
+        System.out.println("Enter inventory quantities:");
+        for (int i = 0; i < n; i++) {
+            inventory[i] = sc.nextInt();
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += inventory[i];
+        }
+
+        double average = (double) total / n;
+
+        System.out.println("Total Inventory: " + total);
+        System.out.printf("Average Inventory: %.2f%n", average);
+
+        System.out.println("Warehouse Status:");
+
+        for (int i = 0; i < n; i++) {
+            if (inventory[i] < average) {
+                System.out.println("Warehouse " + (i + 1) + ": Below Average");
+            } else if (inventory[i] > average) {
+                System.out.println("Warehouse " + (i + 1) + ": Above Average");
+            } else {
+                System.out.println("Warehouse " + (i + 1) + ": Balanced");
+            }
+        }
+
+        sc.close();
+    }
+}
